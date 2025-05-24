eb7c39f00a0e3f7346af1d90cf637319c213444d
Daniil Maykovskiy <daniil.maikovsky@yandex.ru>
2025-05-24 12:41:03 +0300

    upd select padding style

--- src/uikit/select.vue
+++ src/uikit/select.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="border-solid border border-gray-400 relative flex items-center pr-2 w-full">
-    <select class="p-4 px-6 cursor-pointer w-full outline-0" :="$attrs" v-model="model">
+    <select class="p-4 cursor-pointer w-full outline-0" :="$attrs" v-model="model">
       <option v-for="option in options" :key="option.id" :value="option.id">
         {{ option.displayName || 'Не выбрано' }}
       </option>
