addaf23e01431bfdd2e7dfc0baab7933fe87dfad
Daniil Maykovskiy <daniil.maikovsky@yandex.ru>
2025-05-27 00:56:20 +0300

    upd styles

--- src/uikit/button.vue
+++ src/uikit/button.vue
@@ -1,12 +1,12 @@
 <template>
   <button
-    :class="`
-    p-4 px-8 rounded-2xl
-    text-sm
-    focus:outline-2 outline-offset-2
-    cursor-pointer
-    ${twCSS}
-    `"
+    :class="[
+      'p-4 px-8 rounded-2xl',
+      'text-sm',
+      'focus:outline-2 outline-offset-2',
+      'cursor-pointer',
+      ...twCSS,
+    ]"
   >
     <slot></slot>
   </button>
@@ -22,50 +22,50 @@ const { styleStrategy = 'primary' } = defineProps<{
 const twCSS = computed(() => {
   switch (styleStrategy) {
     case 'danger': {
-      return `
-        bg-red-600 hover:bg-red-700 active:bg-red-800
-        text-white
-        focus:outline-red-600
-        shadow-xl/20
-      `
+      return [
+        'bg-red-600 hover:bg-red-700 active:bg-red-800',
+        'text-white',
+        'focus:outline-red-600',
+        'shadow-xl/20',
+      ]
     }
     case 'confirm': {
-      return `
-        bg-green-500 hover:bg-green-600 active:bg-green-700
-        text-white
-        focus:outline-green-500
-        shadow-xl/20
-      `
+      return [
+        'bg-green-500 hover:bg-green-600 active:bg-green-700',
+        'text-white',
+        'focus:outline-green-500',
+        'shadow-xl/20',
+      ]
     }
     case 'neutral': {
-      return `
-        bg-white dark:bg-gray-800
-        hover:bg-gray-100 hover:outline-gray-100
-        dark:hover:bg-gray-800
-        text-gray-900 dark:text-gray-100
-        outline-2
-        outline-white dark:outline-gray-800 
-        focus:outline-gray-300
-        -outline-offset-3!
-        shadow-xl/20
-        active:outline-0
-        active:bg-gray-200 dark:active:bg-gray-900
-        disabled:opacity-60
-        disabled:cursor-not-allowed
-        disabled:outline-0
-        disabled:bg-white
-        disabled:dark:bg-gray-800
-      `
+      return [
+        'bg-white dark:bg-gray-800',
+        'hover:bg-gray-100 hover:outline-gray-100',
+        'dark:hover:bg-gray-800',
+        'text-gray-900 dark:text-gray-100',
+        'outline-2',
+        'outline-white dark:outline-gray-800',
+        'focus:outline-gray-300',
+        '-outline-offset-3!',
+        'shadow-xl/20',
+        'active:outline-0',
+        'active:bg-gray-200 dark:active:bg-gray-900',
+        'disabled:opacity-60',
+        'disabled:cursor-not-allowed',
+        'disabled:outline-0',
+        'disabled:bg-white',
+        'disabled:dark:bg-gray-800',
+      ]
     }
     case 'primary':
     default: {
-      return `
-        bg-violet-500 hover:bg-violet-600 active:bg-violet-700
-        dark:bg-violet-700 dark:hover:bg-violet-600 dark:active:bg-violet-500
-        text-white
-        focus:outline-violet-500
-        shadow-xl/20
-      `
+      return [
+        'bg-violet-500 hover:bg-violet-600 active:bg-violet-700',
+        'dark:bg-violet-700 dark:hover:bg-violet-600 dark:active:bg-violet-500',
+        'text-white',
+        'focus:outline-violet-500',
+        'shadow-xl/20',
+      ]
     }
   }
 })
--- src/uikit/input.vue
+++ src/uikit/input.vue
@@ -1,13 +1,13 @@
 <template>
   <label
-    :class="`
-    w-full
-    cursor-text rounded
-    border
-    bg-white dark:bg-gray-900
-    border-gray-400 dark:border-gray-900
-    focus-within:outline-violet-500 focus-within:outline-2 focus-within:border-violet-500
-    `"
+    :class="[
+      'w-full',
+      'cursor-text rounded',
+      'border',
+      'bg-white dark:bg-gray-900',
+      'border-gray-400 dark:border-gray-900',
+      'focus-within:outline-violet-500 focus-within:outline-2 focus-within:border-violet-500',
+    ]"
   >
     <input
       class="w-full p-4 outline-0 dark:text-gray-100 placeholder:text-gray-400"
