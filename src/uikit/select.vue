f5385a1bd9f3885e74c5fe950b1def001f3b4dcf
Daniil Maykovskiy <daniil.maikovsky@yandex.ru>
2025-05-27 00:14:16 +0300

    upd select

--- src/uikit/select.vue
+++ src/uikit/select.vue
@@ -3,6 +3,7 @@
     <button
       ref="select-btn"
       :class="`
+      group
       w-full
       flex items-center
       p-4 border
@@ -12,26 +13,41 @@
       focus:outline-violet-500 focus:outline-2 focus:border-violet-500
       `"
     >
-      <span v-if="currentDisplayName" class="text-gray-950 dark:text-white">
+      <div v-if="currentDisplayName" class="text-gray-950 dark:text-white">
         {{ currentDisplayName }}
-      </span>
-      <span v-else class="text-gray-400">Не выбрано</span>
+      </div>
+      <div v-else class="text-gray-400">Не выбрано</div>
+      <div
+        :class="{
+          'absolute top-2/4 -translate-1/2 right-3': true,
+          'w-4 h-4': true,
+          'before:block before:w-1 before:h-3.5': true,
+          'before:absolute before:top-1/4 before:-translate-1/2 before:right-1': true,
+          'before:-rotate-45': true,
+          'before:bg-gray-500': true,
+          'before:rounded': true,
+          'after:block after:w-1 after:h-3.5': true,
+          'after:absolute after:top-3/4 after:-translate-1/2 after:right-1': true,
+          'after:rotate-45': true,
+          'after:bg-gray-500': true,
+          'after:rounded': true,
+          'rotate-90': hasDropdown,
+        }"
+      ></div>
     </button>
     <div
       ref="select-dropdown"
-      :class="`
-      absolute w-full
-      hidden
-      px-2 py-1
-      cursor-default rounded
-      bg-gray-900
-      `"
+      :class="{
+        'absolute w-full px-2 py-1 cursor-default rounded bg-gray-900': true,
+        hidden: !hasDropdown,
+      }"
     >
       <template v-for="option in options" :key="option.id">
         <button
           v-if="option.displayName"
           ref="select-options"
-          :class="`w-full
+          :class="`
+          w-full
           cursor-pointer rounded
           outline-0
           px-4 py-2 my-1
@@ -49,8 +65,8 @@
 </template>
 
 <script setup lang="ts">
-import { useEventListener } from '@vueuse/core'
-import { computed, useTemplateRef } from 'vue'
+import { useEventListener, useToggle } from '@vueuse/core'
+import { computed, useTemplateRef, ref, nextTick } from 'vue'
 
 const { options } = defineProps<{
   options: Array<{ id: string; displayName: string }>
@@ -81,6 +97,9 @@ const button = useTemplateRef('select-btn')
 const dropdown = useTemplateRef('select-dropdown')
 const refOptions = useTemplateRef<HTMLButtonElement[]>('select-options')
 
+const hasDropdown = ref(false)
+const toggleHasDropdown = useToggle(hasDropdown)
+
 const selectOption = (optionId: string) => {
   model.value = optionId
 
@@ -88,15 +107,16 @@ const selectOption = (optionId: string) => {
     return
   }
 
-  dropdown.value.classList.toggle('hidden')
+  toggleHasDropdown()
 }
 
-useEventListener(button, 'click', () => {
+useEventListener(button, 'click', async () => {
   if (!dropdown.value) {
     return
   }
 
-  dropdown.value.classList.toggle('hidden')
+  toggleHasDropdown()
+  await nextTick()
 
   if (refOptions.value) {
     refOptions.value[0]?.focus()
@@ -108,8 +128,10 @@ useEventListener(document, 'click', (evt) => {
     return
   }
 
-  if (!button.value.contains(evt.target as Node) && !dropdown.value.contains(evt.target as Node)) {
-    dropdown.value.classList.add('hidden')
+  const target = evt.target as Node
+
+  if (!button.value.contains(target) && !dropdown.value.contains(target)) {
+    hasDropdown.value = false
   }
 })
 </script>
