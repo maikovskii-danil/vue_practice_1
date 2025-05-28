5f0725a3acd888dbbebf13455c7f23749c8bbed0
Daniil Maykovskiy <daniil.maikovsky@yandex.ru>
2025-05-28 15:21:58 +0300

    upd Status

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -7,7 +7,7 @@
       :="$attrs"
     >
       <div
-        class="bg-white dark:bg-gray-700 inline-block absolute top-0 left-0 right-0 bottom-0 rounded-xl opacity-50"
+        class="bg-white dark:bg-gray-700 inline-block absolute top-0 left-0 right-0 bottom-0 rounded-xl"
       ></div>
       <div class="relative z-1">{{ statusDisplayName }}</div>
     </div>
@@ -54,7 +54,7 @@ const twStyle = computed(() => {
       return 'border-red-500 text-red-500'
     }
     case 'in_progress': {
-      return 'border-yellow-700 text-yellow-700 dark:border-yellow-500 dark:text-yellow-500'
+      return 'border-yellow-500 text-yellow-500'
     }
     default: {
       return 'border-green-700 text-green-700 dark:border-green-500 dark:text-green-500'
